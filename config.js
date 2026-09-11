cc0d5c073f95d9ccfa064191b04aeb34e2fb1769
renittournament <khanmdrenit@gmail.com>
2026-09-11 10:36:42 +0600

    Update Supabase key in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
   "https://jeeuayoejozljsjrjtel.supabase.co";
 
 const SUPABASE_KEY =
-  "তোমার আগের Supabase Publishable Key এখানে বসাও";
+  "sb_publishable_zrisa5NAiqdqhAVuJMwHJw_ntoFKYKW";
 
 const supabaseClient =
   window.supabase.createClient(
